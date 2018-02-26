bacc8a6e946ccaac9c106a0d8d70e206b060bc5c
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-02-26 23:51:49 +0000

    Added admin functionality - models should all be registered, please look this over, someone

--- market/admin.py
+++ market/admin.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from rango.models import UserProfile, Item, Offer, Session, OfferContent, SessionParticipants 
 # Register your models here.
+
+
+# Walter 26.2.2018
+# Someone have a think about if we need a UserAdmin class
+#class UserAdmin(admin.ModelAdmin):
+#    pass 
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['posterID', 'currentOwnerID', 'itemName', 'itemDatePosted']
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['offerID', 'fromID', 'toID', 'message', 'offerTimeStamp']
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['sessionID','sessionName', 'sessionStart', 'sessionEnd']
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(UserProfile)
+admin.site.register(OfferContent)
+admin.site.register(SessionParticipants)
