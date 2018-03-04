b66d1148f59cb4ee7e564282133fa68e75cd606f
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-03-04 20:30:09 +0000

    Fixed the templating issue. Need to change a setting, and also set up template tags. Now we can make templates to our hearts content! Have a look at the templatetags folder, I believe we can store a lot of the template rendering logic in this

--- MarketDays/settings.py
+++ MarketDays/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'MarketDays.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
