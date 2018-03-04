bea8b063fd88b19a71addae280a4e54beff471fb
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-03-04 12:52:01 +0000

    wrote some more tests for models, we need to make some for forms, the LoginForm and SignUpForm

--- market/tests.py
+++ market/tests.py
@@ -30,24 +30,43 @@ class ModelTests(TestCase):
             # accept only 1 keyword argument
             if ((len(keys)>1) or (len(keys)==0)):
                 return None
-            elif (keys[0]=="userID"): # if searching by userID
-                user = UserProfile.objects.get(userID=kwargs[keys[0]])
-            elif (keys[0]=="userName"):
-                user = UserProfile.objects.get(userName=kwargs[keys[0]])
-            elif (keys[0]=="firstName"):
-                user = UserProfile.objects.get(firstName=kwargs[keys[0]])
+            key = keys[0]
+            if (key=="userID"): # if searching by userID
+                user = UserProfile.objects.get(userID=kwargs[key])
+            elif (key=="userName"):
+                user = UserProfile.objects.get(userName=kwargs[key])
+            elif (key=="firstName"):
+                user = UserProfile.objects.get(firstName=kwargs[key])
+            elif (key=="lastName"):
+                user = UserProfile.objects.get(lastName=kwargs[key])
             else:
-                print "Unrecognized kwarg"
+                print "Unrecognized kwarg in get_user"
                 return None
         except UserProfile.DoesNotExist:
             user = None
         return user
-
-
     
+    def get_item(self, **kwargs):
+        from market.models import Item
+        try:
+            keys=kwargs.keys()
+            if((len(keys)==0) or (len(keys)>1)):
+                return None
+            key = keys[0]
+            if (key=="itemID"):
+                item = Item.objects.get(itemID=kwargs[key])
+            elif (key=="itemName"):
+                item = Item.objects.get(itemName=kwargs[key])
+            else:
+                print "Unrecognized kwarg in get_item"
+                return None
+        except Item.DoesNotExist:
+            item = None
+        return item
 
     # suite of Model tests
-
+    # tests for UserProfile
+    # test existence
     def test_is_a_user(self):
         # just chose a random userID that you know should be in the db
         user1 = self.get_user(userID = 978)
@@ -58,5 +77,40 @@ class ModelTests(TestCase):
         # choose a first name you know should be in the db
         user3 = self.get_user(firstName = 'Roger')
         self.assertIsNotNone(user3)
+        # choose a lasat name you know should be in the db
+        user4 = self.get_user(lastName = 'Tomlett')
+        self.assertIsNotNone(user4)
+    # tests for Item
+    # test existence
+    # 
+    def test_is_an_item(self):
+        item1 = self.get_item(itemID = 1)
+        self.assertIsNotNone(item1)
+        item2 = self.get_item(itemName = "chin")
+        self.assertIsNotNone(item2)
 
 
+
+
+
+
+
+
+
+
+
+class FormTests(TestCase):
+    def setUp(self):
+        try:
+            from forms import LoginForm # we need to have a login form
+            from forms import SignUpForm # we need to have a signup form
+            from forms import AccountEditForm # we need to be able to edit account
+        except ImportError:
+            print "the forms modele does not exist"
+        except NameError:
+            print "The class []Form does not exist, or is incorrect"
+        except:
+            print "Something else went wrong in FormTests"
+
+#    def test_does_login_form_exist(self):
+        
