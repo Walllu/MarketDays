e3bf2991b6030ff43867e78ae6f61b0de3b95e69
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-03-06 17:11:45 +0000

    quick fix - incorrect references

--- MarketDays/urls.py
+++ MarketDays/urls.py
@@ -23,7 +23,7 @@ from registration.backends.simple.views import RegistrationView
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, user):
-        return '/rango/'
+        return '/market/'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
