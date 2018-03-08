23664a86c47fe9ee6e193bf27ab634edd5be2a3e
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-03-08 19:25:43 +0000

    Added slug fields to the appropriate Models - shouldnt break anything

--- market/models.py
+++ market/models.py
@@ -19,8 +19,11 @@ class UserProfile(models.Model):
     userDescription = models.CharField(max_length=512, unique=True)
     userInterests = models.CharField(max_length=512, unique=True)
     userStartDate = models.DateField(_("Date"), default=datetime.date.today) # Ole, 1st Mar
+    slug = models.SlugField(unique=True)
     #creditcard to model later
-
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(UserProfile, self).save(*args, **kwargs)
     def __str__(self):
         return str(self.userID)
 
@@ -32,7 +35,12 @@ class Item(models.Model):
     itemName = models.CharField(max_length=128)
     itemDescription = models.CharField(max_length=512, unique=True)                   #Why is this unique? Surely we can have non-unique descriptions  Walter 26.2.2018
     itemDatePosted = models.DateField(_("Date"), default=datetime.date.today)
+    slug = models.SlugField(unique=True)
     #itemValue
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Item, self).save(*args, **kwargs
 
     def __str__(self):
         return str(self.itemID)
@@ -43,11 +51,15 @@ class Offer(models.Model):
     fromID = models.ForeignKey(UserProfile, related_name='offer_maker', on_delete=models.PROTECT)
     toID = models.ForeignKey(UserProfile, related_name='offer_reciever', on_delete=models.CASCADE)               #why is this CASCADE?   Walter 26.2.2018
     message = models.CharField(max_length=256)
-    offerTimeStamp = models.DateField(_("Date"), default=datetime.date.today)  
-     
+    offerTimeStamp = models.DateField(_("Date"), default=datetime.date.today)
+    slug = models.SlugField(unique=True)
+
     class Meta:
         verbose_name_plural = 'offers'
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Offer, self).save(*args, **kwargs
     #def __str__(self):
         #Walter 26.2.2018 Added a few of these __str__ classes - I aimed to make them meaningful
      #   return str(self.offerID)+"-from-"+str(self.fromID)+"-to-"+str(self.toID)+"-@"+str(self.offerTimeStamp)
@@ -61,10 +73,14 @@ class Session(models.Model):
     sessionStart = models.DateField(_("Date"), default=datetime.date.today)
     sessionEnd = models.DateField(_("Date"), default=datetime.date.today)
     participants = models.IntegerField(default=0) # Ole, 1st Mar
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name_plural = 'sessions'
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Session, self).save(*args, **kwargs
     #def __str__(self):
     #    return str(self.sessionID)+"-title-"+str(self.sessionName)
 
@@ -78,13 +94,10 @@ class OfferContent(models.Model):
    # def __str__(self):
    #     return str(self.callerID)+"-"str(self.calleeID)+"-"+str(self.itemID)+"-"+str(self.offerID)
 
-    
+
 class SessionParticipants(models.Model):
     sessionID = models.ForeignKey(Session, on_delete=models.PROTECT)
     participantID = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
 
     #def __str__(self):
     #    return str(self.sessionID)+"-participant-"+str(self.participantID)
-    
-         
-    
