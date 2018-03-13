0a4a4184be2917cb7fc00bfa5f757a70ac9e2f4e
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-03-13 22:38:04 +0000

    Added itemlists functionality - just include the relevant template tag with parameters into the templates you already have, and these should take care of the rest in principle. More work to be done on these, implementing drag and drop, etc. etc.

--- market/templatetags/market_template_tags.py
+++ market/templatetags/market_template_tags.py
@@ -12,19 +12,35 @@ register = template.Library()
 def get_session_list():
     return {'sessions': Session.objects.all()}
 
-#I'm not entirely certain yet, but this might be a good place to put
-#the query of getting all your items
-# both for your account view
-# but also for the haggle view
+# these methods will be used to render areas of the site with a certain list of
+# tradable items, called depending on context
+@register.inclusion_tag('market/items.html')
+def get_your_items(yourID):  #this method should return all tradable and nontradable Items
+    return {'yourtradable': Item.objects.filter(claimantID__exact=yourID), 'yournontradable': Item.objects.filter(possessorID__exact=yourID).exclude()}
 
-#@register_inclusion_tag('market/your_items.html')
-#def get_your_items(yourID):
-    #return {'youritems': Item.objects.filter(--)
+@register.inclusion_tag('market/items.html')  # This method returns all tradable items of other user
+def get_their_items(theirID):
+    return {'theirtradable': Item.objects.filter(claimantID__exact=theirID)}
 
+"""
+@register.inclusion_tag('market/items.html')
+def get_items_by_ownership(userID):
+    return {}
+"""
 
-
-#  Some suggested templatetags  --Walter 4.3.2018
-#def get_their_items(theirID)
-#def get_all_items():
-#    return {'sessionitems': Item.objects.all()}
-#def
+@register.inclusion_tag('market/items.html')
+def get_all_items(sessionID):
+    # this method should return all items that are in a session
+    # essentially all the items that are in the inventories of the user within a session
+    session_participants = SessionParticipants.objects.filter(sessionID__exact=sessionID).values('participantID')     # get the userID of all participants - store as a QuerySet
+    session_items = [] # this list will contain Item objects
+    if session_participants.len()==0: # quick check to see if there's any participants to begin with
+        return {'sessionitems':None}
+    else: # some participants exist
+        for u in session_participants:
+            uid = u['participantID'] # ID of user
+            uitems = Item.objects.filter(possessorID=uid) # a QuerySet containing Item objects
+            for item in uitems:
+                session_items.append(item)
+    return {'sessionitems': session_items}
+    # NOTE: I think it should be fine passing a list - the important thing is that it's an iterable
