95cc401bd0e764896c1b22371d8ca5886af79c89
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-03-18 13:44:25 +0000

    fixed some issued with importing jquery and jquery ui - added jquery ui theme (which we can change if you guys dontt like the colors or smth) and started dabbling with jquery - expect more soon

--- static/js/scripts/ui.js
+++ static/js/scripts/ui.js
@@ -1,1 +1,7 @@
 // this is where we should put in the jQuery and jQuery UI for offer notifications
+
+$(document).ready(function(){
+    // put jquery ui functionality here
+    $(".ui-widget-content").draggable({revert:true});
+  }
+);
